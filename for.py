5b4bf3ce006d3d37cefcf7a01913bdebbd1da8e4
NetAcad DEVASC <changeme@example.com>
2021-04-13 18:02:24 +0000

    version paarte1

--- for.py
+++ for.py
@@ -3,11 +3,11 @@ for i in range(a):
     print(i, end=' ')
 print('\n')
 
-print('programa modificado version1')
-
 lista=[i for i in range(5)]
 if [3,4] in lista:
     print('Mis numeros')
 else:
     print('No se ubica')
+
+print('programa modificado version1 parte1')
 

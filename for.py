adb7fd140e1c66e32e77fdccad13b8dd5e609579
NetAcad DEVASC <changeme@example.com>
2021-04-13 17:51:44 +0000

    Optimizando for.py

--- for.py
+++ for.py
@@ -4,3 +4,10 @@ for i in range(a):
 print('\n')
 
 print('programa modificado version1')
+
+lista=[i for i in range(5)]
+if [3,4] in lista:
+    print('Mis numeros')
+else:
+    print('No se ubica')
+

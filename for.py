c2e222091541ce703c68a16a0a038d9c17116f59
NetAcad DEVASC <changeme@example.com>
2021-04-13 21:14:54 +0000

    Error

--- for.py
+++ for.py
@@ -1,8 +1,3 @@
-a=10
-for i in range(a):
-    print(i, end=' ')
-print('\n')
-
 lista=[i for i in range(5)]
 if [3,4] in lista:
     print('Mis numeros')
@@ -10,17 +5,16 @@ else:
     print('No se ubica')
 
 Dic={}
-Dic[1]='uno'
-Dic[2]='dos'
-
-print(Dic)
+Dic['Andres']=(1,2)
+Dic['Beatriz']=(3,4)
+Dic['Carlos']=(5,6)
 
 #formato de table
 Mark = '+---------------+----------+----------+'
-Row = '\n| Ciudad        | Latitud  | Longitud |\n'
+Row = '\n| Nombre        | Latitud  | Longitud |\n'
 print(Mark+Row+Mark)
-for i in DB.keys():
-    print("| {0:<13} | {1:<8.4f} | {2:<8.4f} |".format(i,DB[i][0],DB[i][1]))
+for i in Dic.keys():
+    print("| {0:<13} | {1:<8.4f} | {2:<8.4f} |".format(i,Dic[i][0],Dic[i][1]))
 print(Mark,'\n\n\n')
 
 print('programa modificado Devent branch')
